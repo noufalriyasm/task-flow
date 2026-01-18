a6df81385818b6284582dd70efd5080525ffa072
noufalriyasm <makkatnoufalriyas@gmail.com>
2026-01-18 17:44:37 +0530

    authetication

--- src/main/java/com/taskFlow/controller/CompanyController.java
+++ src/main/java/com/taskFlow/controller/CompanyController.java
@@ -10,5 +10,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/v1/api/task-flow/company/")
 public class CompanyController {
-    //adding qa branch
+    //adding complete authentication
 }
